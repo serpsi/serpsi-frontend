a72eb1c2e573838f91e8d25e33bfc8ede9c76741
Antônio César <antonioazevedo.dev@gmail.com>
2024-09-01 22:00:08 -0300

    ✨feat: adição da fonte e tema de cores

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,19 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "login": "url('/src/app/login/login-bg.png')",
+        "gradiente-vidro": "linear-gradient(90.14deg, rgba(0, 153, 216, 0.22) 32.76%, rgba(224, 243, 254, 0.2) 61.12%)",
       },
+      colors: {
+        "vidro": "#E0F3FE",
+        "primary-950": "#07324A",
+        "primary-600": "#0099D8",
+        "primary-500": "#0CB1EB",
+        "primary-50": "#F0FAFF",
+      },
+      borderColor: {
+        "primary-500": "#0CB1EB"
+      }
     },
   },
   plugins: [],
