0fcaaeab84519bfcc56c2623fa58381ef137b942
Antônio César <antonioazevedo.dev@gmail.com>
2024-09-16 22:42:12 -0300

    feat: Tela Login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,49 +1,51 @@
+import { Button } from "@/components/form/button";
 import { InputText } from "@/components/form/input";
 
 export default function LoginPage() {
-  return (
-    <main
-      className="flex h-screen w-screen bg-cover p-10 md:items-center md:justify-center"
-      style={{ backgroundImage: "url('/img/login-bg.jpg')" }}
-    >
-      <section className="borderborder-primary-500 flex h-full w-4/5 flex-row items-center justify-around rounded-[30px] bg-gradiente-vidro p-9">
-        <section className="hidden w-2/5 text-primary-950 lg:block">
-          <h2 className="text-3xl">Gerencie o seu consultório de psicologia</h2>
-          <p className="text-xl">
-            Horários, pacientes, documentos, tudo num só lugar.
-          </p>
-          <button className="mt-2 w-full rounded-md bg-primary-600 p-3 text-sm text-primary-50">
-            Saiba Mais
-          </button>
-        </section>
-        <section className="w-full text-primary-950 lg:w-2/5">
-          <h1 className="text-primary-900 mb-2 text-center text-3xl font-medium">
-            Login
-          </h1>
-          <form>
-            <InputText
-              type="text"
-              label="Insira seu E-mail"
-              id="email"
-              placeholder="Email"
-            />
-            <br />
-            <InputText
-              type="password"
-              label="Insira sua Senha"
-              id="password"
-              placeholder="Senha"
-            />
-            <br />
-            <button
-              type="submit"
-              className="text-primary-700 mt-2 w-full rounded-xl border border-primary-500 bg-primary-50 pb-1 pt-1 text-xl"
-            >
-              Entrar
-            </button>
-          </form>
-        </section>
-      </section>
-    </main>
-  );
+	return (
+		<main
+			className="flex h-screen w-screen items-center justify-center bg-cover p-5 md:p-10"
+			style={{ backgroundImage: "url('/img/login-bg.jpg')" }}
+		>
+			<section className="flex h-full w-full flex-row items-center justify-around rounded-[30px] border border-primary-500 bg-gradiente-vidro p-9 md:w-4/5">
+				<section className="hidden w-2/5 text-primary-950 lg:block">
+					<h2 className="text-5xl">
+						Gerencie o seu consultório de psicologia
+					</h2>
+					<p className="mb-4 mt-8 text-2xl">
+						Horários, pacientes, documentos,
+						<br /> tudo num só lugar.
+					</p>
+					<Button className="mt-2 p-3 text-sm" text="Saiba mais" />
+				</section>
+				<section className="w-full text-primary-950 lg:w-2/5">
+					<h1 className="mb-8 text-center text-5xl font-medium text-primary-900">
+						Login
+					</h1>
+					<form className="flex h-2/5 flex-col justify-around">
+						<InputText
+							type="text"
+							label="Insira seu E-mail"
+							id="email"
+							placeholder="Email"
+						/>
+						<br />
+						<InputText
+							type="password"
+							label="Insira sua Senha"
+							id="password"
+							placeholder="Senha"
+						/>
+						<br />
+						<Button
+							text="Entrar"
+							variant="second"
+							className="pb-1 pt-1 text-xl"
+							type="submit"
+						/>
+					</form>
+				</section>
+			</section>
+		</main>
+	);
 }
--- src/components/form/input.tsx
+++ src/components/form/input.tsx
@@ -1,25 +1,25 @@
 interface InputTextProps {
-  id: string;
-  label: string;
-  placeholder: string;
-  type: "text" | "password";
+	id: string;
+	label: string;
+	placeholder: string;
+	type: "text" | "password";
 }
 
 export function InputText(props: InputTextProps) {
-  return (
-    <>
-      <label
-        htmlFor={props.id}
-        className="w-full text-sm font-medium text-primary-950"
-      >
-        {props.label}
-      </label>
-      <input
-        id={props.id}
-        type={props.type}
-        placeholder={props.placeholder}
-        className="text-primary-800 focus:outline-primary-800 w-full rounded-xl border border-primary-500 bg-vidro p-2 focus:outline"
-      />
-    </>
-  );
+	return (
+		<>
+			<label
+				htmlFor={props.id}
+				className="mb-1 w-full text-sm font-medium text-primary-950"
+			>
+				{props.label}
+			</label>
+			<input
+				id={props.id}
+				type={props.type}
+				placeholder={props.placeholder}
+				className="w-full rounded-xl border border-primary-500 bg-vidro p-2 text-primary-800 focus:outline focus:outline-primary-800"
+			/>
+		</>
+	);
 }
