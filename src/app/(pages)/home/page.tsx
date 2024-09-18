8e4a0a0607d1168676d549249403d8e7d3305274
Antônio César <antonioazevedo.dev@gmail.com>
2024-09-17 22:53:22 -0300

    feat: estrutura NavigationLayout Inicial

---
+++ src/app/(pages)/home/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between bg-indigo-200 p-24">
+			<h1>Olá mundo! Estou na Home.</h1>
+		</main>
+	);
+}
