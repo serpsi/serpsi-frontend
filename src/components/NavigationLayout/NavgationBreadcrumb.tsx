93b6fd507eb82e5c487be5653c8506508bb59b91
Antônio César <antonioazevedo.dev@gmail.com>
2024-09-24 21:47:09 -0300

    🔥hotfix: ajuste breadcrumb

--- src/components/NavigationLayout/NavgationBreadcrumb.tsx
+++ src/components/NavigationLayout/NavgationBreadcrumb.tsx
@@ -26,20 +26,25 @@ const routeTitles: { [key: string]: string } = {
 };
 
 export default function NavigationBreadcrumb() {
-	var urlpath: string = usePathname();
+	const urlpath: string = usePathname();
 
 	const getCrumbs = (): TRoute[] => {
-		if (!urlpath.includes("/home")) {
-			urlpath = "/home" + urlpath;
-		}
-
 		const parts = urlpath.split("/").filter(Boolean);
 
-		const crumbs: TRoute[] = parts.map((part, index) => {
+		var crumbs: TRoute[] = parts.map((part, index) => {
 			const link = "/" + parts.slice(0, index + 1).join("/");
 			const title = routeTitles[part] || part;
 			return { title, link };
 		});
+
+		if (
+			!crumbs.some(
+				(crumb) => crumb.title === "Início" && crumb.link === "/home"
+			)
+		) {
+			crumbs = [{ title: "Início", link: "/home" }, ...crumbs];
+		}
+
 		return crumbs;
 	};
 
