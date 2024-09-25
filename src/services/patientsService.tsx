99e7812b63a91db549904f047ad35086c60b6723
Danielbb21 <50447725+Danielbb21@users.noreply.github.com>
2024-09-25 19:42:16 -0300

    :recycle: refactor: ajustandpo rota de listar todos os pacientes e aceitando qualquer protocolo para imagem
    Co-Authored-By: João Pedro de Lima <48960188+jotalima72@users.noreply.github.com>

--- src/services/patientsService.tsx
+++ src/services/patientsService.tsx
@@ -1,6 +1,6 @@
 export async function getData() {
 	'use server'
 	const response = await fetch(
-		process.env.BACKEND_URL + "/patients/psychologist");
+		process.env.BACKEND_URL + "/patients/psychologist", {method: 'GET'});
 	return response.json();
 }
