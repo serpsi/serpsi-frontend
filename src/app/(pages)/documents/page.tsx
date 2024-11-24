63261c143c2127306f76ab70994cca2bfc1bcf45
João Pedro de Lima <48960188+jotalima72@users.noreply.github.com>
2024-11-24 00:30:03 -0300

    :fire: hotfix: consertando listagem de documents que tinha quebrado
    Co-Authored-By: Antônio César <antonioazevedo.dev@gmail.com>

--- src/app/(pages)/documents/page.tsx
+++ src/app/(pages)/documents/page.tsx
@@ -1,17 +1,12 @@
 import { DataTable } from "@/components/table/data-table";
-import { columns, data } from "./columns";
-import { Document } from "@/models";
-
-async function getData(): Promise<Document[]> {
-	const response = data;
-	return response;
-}
+import { columns } from "./columns";
+import { getData } from "@/services/documentsService";
 
 export default async function Documents() {
 	const data = await getData();
 	return (
 		<main className="flex h-full w-full items-center justify-center bg-white p-3">
-			<DataTable columns={columns} data={data} filteringColumn="name" filteringPlaceHolder="nome" />
+			<DataTable columns={columns} data={data!} filteringColumn="name" filteringPlaceHolder="nome" />
 		</main>
 	);
 }
